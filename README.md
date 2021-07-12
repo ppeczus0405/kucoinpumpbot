da05b5095aa210fc0aa241229b19ca79405278bd
Patryk Peczak <ppeczak0405@gmail.com>
2021-07-12 22:20:40 +0200

    Update README.md and coinfig.py

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 It's a kucoin bot for earn money on pumps.
 
 To start earn money (: you have to:
-- generate API keys(key, secret, passphrase) and put in config file
+- install all dependencies: telethon, requests, python-kucoin (for example with 'pip3 install dependency')
+- generate telegram and kucoin api keys and paste in config
+- generate trade request header(It's working on the latest version of Mozilla Firefox, I have no idea how with other browsers):
+    - Go to trade section on kucoin(you have to be logged in)
+    - Choose any cryptocurrency you are able to buy
+    - Open Web Developpers Tools and select network tab
+    - Buy cryptocurrency and find request associated
+    - Copy requests header and paste into config file(You have example request header in config)
 - set expected profit
 - before the pump message execute bot(python3 main.py)
--- config.py
+++ config.py
@@ -1,17 +1,27 @@
-import keys
-
 # PUMP
 COIN_USED_TO_PUMP = "USDT"  # The coin for which you will be buying pumped coin
-COINS = 20  # Amount of coins you want use for a pump
-EXPECTED_PROFIT = 50  # If you want to make 150$ from 100$ set EXPECTED_PROFIT = 50
+COINS = 1500  # Amount of coins you want use for a pump
+EXPECTED_PROFIT = 100  # If you want to make 3000$ from 1500$ set EXPECTED_PROFIT = 100
 
 # KUCOIN
-KUCOIN_HEADER = keys.kucoin_header
-KUCOIN_KEY = keys.kucoin_key
-KUCOIN_SECRET = keys.kucoin_secret
-KUCOIN_PASSCODE = keys.kucoin_passphrase
+KUCOIN_HEADER = """POST /_api/trade/orders?c=xx HTTP/1.1
+Host: trade.kucoin.com
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0
+Accept: application/json
+Accept-Language: pl,en-US;q=0.7,en;q=0.3
+Accept-Encoding: gzip, deflate, br
+Content-Type: multipart/form-data; boundary=---------------------------xxx
+Content-Length: 760
+Origin: https://trade.kucoin.com
+DNT: 1
+Referer: https://trade.kucoin.com/MOON-USDT
+Connection: keep-alive
+Cookie: xxx """
+KUCOIN_KEY = "Kucoin Key"
+KUCOIN_SECRET = "Kucoin Secret"
+KUCOIN_PASSCODE = "Kucoin Passcode"
 
 # TELEGRAM
 TELEGRAM_ID = keys.telegram_api_id
 TELEGRAM_HASH = keys.telegram_api_hash
-CHAT_NAME = "Kucoin Pumps"
+CHAT_NAME = "Patryk Pęczak"
